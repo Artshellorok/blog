c41c7e2473b731681c546785853d1d95eefd4ca7
Artshellorok <artshellorok@DESKTOP-L8HFSLF.localdomain>
2018-11-20 20:52:33 +0900

    Initial commit

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,52 @@
+<nav class="navbar navbar-dark bg-primary">
+        <div class='container'>
+            <a class="navbar-brand" href="#">Navbar</a>
+        </div>
+</nav> 
+<div style='height:300px'>
+    <div class='container'>
+        <h1>РИКЛАМА КУПИТЕ УЖЕ СЕГОДНЯ СО СКИДКОЙ 5 ПРАЦИНТАВ</h1>
+    </div>
+</div>
+<nav class="navbar navbar-dark bg-primary navbar-expand-lg">
+    <div class='container'>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav nav-links">
+                <li class="nav-item home current">
+                    <a class="nav-link" href="/">
+                        <img src="img/home.png" width="20" height="20">
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Business</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Travel</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">People</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Sports</a>
+                </li>
+                <li class="nav-item">
+                     <a class="nav-link" href="#">Music</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Forum</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <button type="button" class="btn btn-dark search ml-auto rounded" aria-label="Close">
+            <img src="img/search.png" alt="search" class="rounded">
+        </button>
+    </div>
+</nav>
+<hr class='hr-red'>
+
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.master')
+@section('content')
+<div class='container'>    
+    <div class='row'>
+        <div class='col-sm-3'>
+        </div>
+        <div class='col-sm-9'>
+            
+        </div>
+    </div>
+</div>
+@endsection
